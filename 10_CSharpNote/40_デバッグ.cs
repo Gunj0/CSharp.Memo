7e1f2055b1c59d9b68adebfe82d233979efb1ecf
gunj0 <gunjo2012@gmail.com>
2024-12-09 18:08:24 +0900

    memo更新

---
+++ 10_CSharpNote/40_デバッグ.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+
+namespace CSharp.Memo._10_CSharpNote
+{
+  internal class _40_デバッグ
+  {
+    // DEBUGのときだけ実行
+#if DEBUG
+    int hoge = 10;
+#endif
+
+    // DEBUG以外のときだけ実行
+#if !DEBUG
+    int hoge = 10;
+#endif
+
+    // 自動ウィンドウ: 実行している近くの変数が見れる
+    // ローカルウィンドウ: 現在のメソッド内の引数や変数が見れる
+    // ウォッチウィンドウ: 固定して変数を見れる
+
+    public int Get()
+    {
+      Debug.WriteLine("");
+      return 0;
+    }
+  }
+}
